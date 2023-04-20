102343b9b00ead2e1a5a49ccc041d453bd36199e
Yehan20 <yehansk15@gmail.com>
2023-04-20 14:57:53 +0530

    update

---
+++ src/components/megaMenu.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import MegaMenuStyled, { MenuBigChildStyled, MenuChildStyled1, MenuChildStyled2, MenuChildStyled3, MenuChildStyled4 } from '../styled/megaMenuStyled'
+
+const MegaMenu = () => {
+  return (
+    <MegaMenuStyled>
+
+        <MenuChildStyled1>
+            <p>Straight from the Garden</p>
+            <h3>Fresh Fruits</h3>
+        </MenuChildStyled1>
+
+        <MenuChildStyled2>
+            <p>No Chemicals</p>
+            <h3>Fresh Taste</h3>
+        </MenuChildStyled2>
+
+        <MenuBigChildStyled>
+            <h3>Organic <span>Vegetables and Fruits</span></h3>
+            <p>Sale up to 40% off </p>
+            <p>Best price</p>
+            <Link to='/all' title='Click to visit'>Show now</Link>
+        </MenuBigChildStyled>
+
+        <MenuChildStyled3>
+            <p>Savor the Flavor of Nature's Bounty</p>
+            <h3>Handpicked Vegetables</h3>
+        </MenuChildStyled3>
+
+        <MenuChildStyled4>
+            <p>Best Price</p>
+            <h3>Organic Vegetables</h3>
+        </MenuChildStyled4>
+    </MegaMenuStyled>
+  )
+}
+
+export default MegaMenu
