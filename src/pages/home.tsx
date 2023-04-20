efed52d368d5047ff654edb94c0de2dcf9de4f00
Yehan20 <yehansk15@gmail.com>
2023-04-20 14:57:36 +0530

    add new folder

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/header'
+import MegaMenu from '../components/megaMenu'
+
+const Home = () => {
+  return (
+    <>
+      <MegaMenu/>
+    </>
+  )
+}
+
+export default Home
